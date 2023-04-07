f61bf34afd0f458b4bc6d3f950beb7cec3e6ec78
Gleb Korobov <gkorobov9@gmail.com>
2023-04-07 09:15:17 +0300

    added some changes

--- src/test/java/com/ua/glebkorobov/CreateConnectionWithDBTest.java
+++ src/test/java/com/ua/glebkorobov/CreateConnectionWithDBTest.java
@@ -10,27 +10,27 @@ import static org.mockito.Mockito.*;
 
 class CreateConnectionWithDBTest {
 
-//    @Test
-//    void testCloseConnection() throws SQLException {
-//        CreateConnectionWithDB example = new CreateConnectionWithDB();
-//        Connection mockConnection = mock(Connection.class);
-//
-//        example.closeConnection(mockConnection);
-//
-//        verify(mockConnection, times(1)).close();
-//    }
-//
-//
-//    @Test
-//    void testGetRemoteConnectionMissingValues() {
-//        GetProperty mockProperty = mock(GetProperty.class);
-//        when(mockProperty.getValueFromProperty("rds_hostname")).thenReturn(null);
-//
-//        CreateConnectionWithDB connectionWithDB = new CreateConnectionWithDB();
-//
-//        Connection actual = connectionWithDB.getRemoteConnection(mockProperty);
-//
-//        assertNull(actual);
-//        verify(mockProperty, times(1)).getValueFromProperty("rds_hostname");
-//    }
+    @Test
+    void testCloseConnection() throws SQLException {
+        CreateConnectionWithDB example = new CreateConnectionWithDB();
+        Connection mockConnection = mock(Connection.class);
+
+        example.closeConnection(mockConnection);
+
+        verify(mockConnection, times(1)).close();
+    }
+
+
+    @Test
+    void testGetRemoteConnectionMissingValues() {
+        GetProperty mockProperty = mock(GetProperty.class);
+        when(mockProperty.getValueFromProperty("rds_hostname")).thenReturn(null);
+
+        CreateConnectionWithDB connectionWithDB = new CreateConnectionWithDB();
+
+        Connection actual = connectionWithDB.getRemoteConnection(mockProperty);
+
+        assertNull(actual);
+        verify(mockProperty, times(1)).getValueFromProperty("rds_hostname");
+    }
 }
--- src/test/java/com/ua/glebkorobov/filling_tables/FillTypeTableTest.java
+++ src/test/java/com/ua/glebkorobov/filling_tables/FillTypeTableTest.java
@@ -19,41 +19,41 @@ import static org.mockito.Mockito.*;
 
 class FillTypeTableTest {
 
-//    @Test
-//    void testFill() throws SQLException, IOException, CsvException {
-//        String[] type1 = new String[]{"type1"};
-//        String[] type2 = new String[]{"type2"};
-//
-//        List<String[]> types = Arrays.asList(type1, type2);
-//        Connection connectionMock = mock(Connection.class);
-//        CSVReader csvReaderMock = mock(CSVReader.class);
-//        PreparedStatement statementMock = mock(PreparedStatement.class);
-//
-//        FillTypeTable fillTypesTable = new FillTypeTable();
-//
-//        when(connectionMock.prepareStatement(anyString())).thenReturn(statementMock);
-//        when(csvReaderMock.readAll()).thenReturn(types);
-//
-//        fillTypesTable.fill(connectionMock, csvReaderMock);
-//
-//        verify(statementMock, times(2)).setString(eq(1), anyString());
-//        verify(statementMock, times(2)).setString(eq(2), anyString());
-//        verify(statementMock, times(2)).addBatch();
-//        verify(statementMock).executeBatch();
-//    }
-//
-//    @Test
-//    void testFillWhenFileNotFoundException() throws SQLException, IOException, CsvException {
-//        Connection connectionMock = mock(Connection.class);
-//        CSVReader csvReaderMock = mock(CSVReader.class);
-//        PreparedStatement statementMock = mock(PreparedStatement.class);
-//
-//        FillTypeTable fillTypesTable = new FillTypeTable();
-//
-//        when(connectionMock.prepareStatement(anyString())).thenReturn(statementMock);
-//        when(csvReaderMock.readAll()).thenThrow(new FileNotFoundException());
-//
-//        assertThrows(FileFindException.class, () -> fillTypesTable.fill(connectionMock, csvReaderMock));
-//    }
+    @Test
+    void testFill() throws SQLException, IOException, CsvException {
+        String[] type1 = new String[]{"type1"};
+        String[] type2 = new String[]{"type2"};
+
+        List<String[]> types = Arrays.asList(type1, type2);
+        Connection connectionMock = mock(Connection.class);
+        CSVReader csvReaderMock = mock(CSVReader.class);
+        PreparedStatement statementMock = mock(PreparedStatement.class);
+
+        FillTypeTable fillTypesTable = new FillTypeTable();
+
+        when(connectionMock.prepareStatement(anyString())).thenReturn(statementMock);
+        when(csvReaderMock.readAll()).thenReturn(types);
+
+        fillTypesTable.fill(connectionMock, csvReaderMock);
+
+        verify(statementMock, times(2)).setString(eq(1), anyString());
+        verify(statementMock, times(2)).setString(eq(2), anyString());
+        verify(statementMock, times(2)).addBatch();
+        verify(statementMock).executeBatch();
+    }
+
+    @Test
+    void testFillWhenFileNotFoundException() throws SQLException, IOException, CsvException {
+        Connection connectionMock = mock(Connection.class);
+        CSVReader csvReaderMock = mock(CSVReader.class);
+        PreparedStatement statementMock = mock(PreparedStatement.class);
+
+        FillTypeTable fillTypesTable = new FillTypeTable();
+
+        when(connectionMock.prepareStatement(anyString())).thenReturn(statementMock);
+        when(csvReaderMock.readAll()).thenThrow(new FileNotFoundException());
+
+        assertThrows(FileFindException.class, () -> fillTypesTable.fill(connectionMock, csvReaderMock));
+    }
 
 }
